5eb898a6d6143fa00b8578e2529437da9477c76f
Thrinad123 <yaruvatrinad1997@gmail.com>
2023-02-27 21:18:59 +0000

    added two new configs

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,7 +4,7 @@ from hello import say_hello
 class TestHello(unittest.TestCase):
     def test_say_hello(self):
         ##assert say_hello("Alice") == "Hello, Alice!"
-        assert say_hello("thrinad") != "Hello, Bob!"
+        assert say_hello("thrinad") == "Hello, Bob!"
 
 if __name__ == '__main__':
     unittest.main()
