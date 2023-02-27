843abcb833d110f4971bddde2eddb275d5406d47
Thrinad123 <yaruvatrinad1997@gmail.com>
2023-02-27 20:32:43 +0000

    changed cinfig

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,5 @@
 from hello import say_hello
 
 def test_say_hello():
-    assert say_hello("Alice") == "Hello, Alice!"
-    assert say_hello("Bob") == "Hello, Bob!"
+    ##assert say_hello("Alice") == "Hello, Alice!"
+    assert say_hello("thrinad") == "Hello, Bob!"
