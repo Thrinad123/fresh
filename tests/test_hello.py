32e4524e586e0259eab3264e1f30c09fa9252de0
Thrinad123 <yaruvatrinad1997@gmail.com>
2023-02-27 21:30:23 +0000

    added newigs

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,10 +1,10 @@
-import unittest
-from hello import say_hello
 
-class TestHello(unittest.TestCase):
-    def test_say_hello(self):
-        ##assert say_hello("Alice") == "Hello, Alice!"
-        assert say_hello("thrinad") == "Hello, Bob!"
+from hello import say_hello
+##class TestHello(unittest.TestCase):
+    def test_say_hello():
+        assert say_hello("Alice") == "Hello, Alice!"
+        assert say_hello("bob") == "Hello, Bob!"
+        print("above function is working fine ")
 
 if __name__ == '__main__':
-    unittest.main()
+        test_say_hello()
